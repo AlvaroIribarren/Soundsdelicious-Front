81c37feff8c0aee401976b5a3877b84350fcd40c
Santiago <sberoch@gmail.com>
2023-02-15 11:12:14 -0300

    api url

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 export const config = {
-  // API_URL: "api.soundsdeliciousmusiclibrary.com"
-  API_URL: "161.35.96.12:3030"
+  API_URL: "api.soundsdeliciousmusiclibrary.com"
+  // API_URL: "161.35.96.12:3030"
 };

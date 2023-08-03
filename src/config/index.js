5141ab3cca863d45850e7d8479bad1cbfd898ec4
Santiago Beroch <sberoch@gmail.com>
2023-08-03 12:51:01 -0300

    change videosync url

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export const config = {
   API_URL: "api.soundsdeliciousmusiclibrary.com",
   // API_URL: "161.35.96.12:3030",
-  // VIDEOSYNC_URL: "videosync.soundsdeliciousmusiclibrary.com"
-  VIDEOSYNC_URL: "http://localhost:5000"
+  VIDEOSYNC_URL: "https://videosync.soundsdeliciousmusiclibrary.com"
+  // VIDEOSYNC_URL: "http://localhost:5000"
 };
